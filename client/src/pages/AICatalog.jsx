470d62f83da3d2afffba4636278ca59a7615b6dd
Hassan <hassanhauda@gmail.com>
2024-09-12 02:22:55 +0100

    Refactor grid layout in AICatalog component

--- client/src/pages/AICatalog.jsx
+++ client/src/pages/AICatalog.jsx
@@ -63,7 +63,7 @@ const AICatalog = () => {
               Beta
             </span>
           </div>
-          <div className="grid gap-2 sm:grid-cols-2 md:grid-cols-3">
+          <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3">
             {displayedSuggestions.map((suggestion, index) => (
               <button
                 key={index}
